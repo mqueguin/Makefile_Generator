d3b2d3eabd29ecabcea71e1fba99a0c7cab6a6b2
Martin QUEGUINER <mqueguin@e1r4p13.42.fr>
2021-09-21 15:48:07 +0200

    Final push Makefile Generator

--- test.py
+++ test.py
@@ -14,15 +14,29 @@ cc = raw_input("Tapez le compilateur que vous voulez clang ou gcc : ")
 flags = raw_input("Tapez les flags dont vous avez besoins a la compilation : ")
 msg_compil = raw_input("(Falcutatif) Tapez un message pour la compilation (exemple : [LOADED]) : ")
 libft_compil = input("1 Si tu veux cacher la compilation de la libft avec /dev/null sinon 0 : ")
+bonus = input("Tapez 1 si vous voulez la regle bonus sinon 0 : ")
+if bonus == 1:
+    name_b = raw_input("Le nom du programme bonus : ")
+    srcs_b = raw_input("Tapez le dossier ou sont vos .c bonus : ")
+    includes_b = raw_input("Tapez le dossier ou sont vos .h bonus : ")
+
 
 file = open("Makefile", "w")
 file.write("NAME	        =		" + word)
+if bonus == 1:
+    file.write("\n\nNAME_B          =       " + name_b)
 file.write("\n\nSRCS            =       $(wildcard " + srcs + "/*.c)\n\n")
+if bonus == 1:
+    file.write("SRCS_B      =       $(wildcard " + srcs_b + "/*.c)\n\n")
 file.write("INCLUDES_DIR     =       " + includes + "/\n\n")
+if bonus == 1:
+    file.write("INCLUDES_DIR_B  =       " + includes_b + "/\n\n")
 file.write("LIB_DIR         =       " + libft + "/\n\n")
 file.write("LIBFT           =       " + libft_dir + "\n\n")
 file.write("CC              =       " + cc + "\n\n")
 file.write("OBJS            =       ${SRCS:.c=.o}\n\n")
+if bonus == 1:
+    file.write("OBJS_B          =       ${SRCS_B:.c=.o}\n\n")
 
 if mlx == 1:
     file.write("MLX         =       " + mlxa + "\n\n")
@@ -30,12 +44,12 @@ if mlx == 1:
 
 file.write("RM          =       rm -rf\n\n")
 if mlx == 1:
-    file.write("FLAGS       =       " + flags + " " + flags_mlx_mac + "\n\n")
-else:
-    file.write("FLAGS       =       " + flags + "\n\n")
+    file.write("LFLAGS       =       " + flags_mlx_mac + "\n\n")
+file.write("FLAGS       =       " + flags + "\n\n")
 
-file.write(".c.o:\t\t\t\t@${CC} ${FLAGS} -I${INCLUDES_DIR} -c $< -o $@\n")
-file.write("\t\t\t\t\t@echo \"" + msg_compil + "\"\n\n")
+file.write(".c.o:\n")
+file.write("\t\t\t\t\t@${CC} ${FLAGS} -I${INCLUDES_DIR} -c $< -o ${<:.c=.o}\n")
+file.write("\t\t\t\t\t@echo \"\\x1b[32m" + msg_compil + "\\033[0m ${<:.s=.o}\"\n\n")
 
 file.write("all:\t\t\t${NAME}\n\n")
 file.write("${NAME}:\t\t${OBJS}\n")
@@ -49,12 +63,41 @@ else:
     file.write("\t\t\t\t\t\t@make -C ${LIB_DIR} bonus\n")
 
 if mlx == 1:
-    file.write("\t\t\t\t\t\t@${CC} ${FLAGS} -I${INCLUDES_DIR} -o ${NAME} ${OBJS} ${LIBFT} ${MLX}\n")
+    file.write("\t\t\t\t\t\t@${CC} ${FLAGS} ${LFLAGS} -I${INCLUDES_DIR} -o ${NAME} ${OBJS} ${LIBFT} ${MLX}\n")
 else:
     file.write("\t\t\t\t\t\t@${CC} ${FLAGS} -I${INCLUDES_DIR} -o ${NAME} ${OBJS} ${LIBFT}\n")
-file.write("\t\t\t\t\t\t@echo \"libft.a has been created\"\n")
+file.write("\t\t\t\t\t\t@echo \"\\nlibft.a has been created\"\n")
 file.write("\t\t\t\t\t\t@echo \"" + word + " has been created\"\n\n")
-file.write("\n.PHONY:           all     clean   fclean  re\n")
+
+if bonus == 1:
+    file.write("bonus:\t\t${OBJS_B}\n")
+    if mlx == 1:
+        file.write("\t\t\t\t\t\t@make -C ${MLX_DIR} 2>/dev/null\n")
+    if libft_compil == 1:
+        file.write("\t\t\t\t\t\t@make -C ${LIB_DIR} 2>/dev/null\n")
+        file.write("\t\t\t\t\t\t@make -C ${LIB_DIR} bonus 2>/dev/null\n")
+    else:
+        file.write("\t\t\t\t\t\t@make -C ${LIB_DIR}\n")
+        file.write("\t\t\t\t\t\t@make -C ${LIB_DIR} bonus\n")
+    if mlx == 1: 
+        file.write("\t\t\t\t\t\t@${CC} ${FLAGS} ${LFLAGS} -I ${INCLUDES_DIR_B} -o ${NAME_B} ${OBJS_B} ${LIBFT} ${MLX}\n")
+    else:
+        file.write("\t\t\t\t\t\t@${CC} ${FLAGS} -I ${INCLUDES_DIR_B} -o ${NAME_B} ${OBJS_B} ${LIBFT}\n")
+    file.write("\t\t\t\t\t\t@echo \"\\nlibft.a has been created\"\n")
+    file.write("\t\t\t\t\t\t@echo \"" + name_b + " has been created\"\n\n")
+
+file.write("clean:\n")
+file.write("\t\t\t\t\t\t@make -C ${LIB_DIR} clean\n")
+file.write("\t\t\t\t\t\t@${RM} ${OBJS} ${OBJS_B}\n")
+file.write("\t\t\t\t\t\t@echo \"\\n *.o files deleted\\n\"\n\n")
+
+file.write("fclean:\t\tclean\n")
+file.write("\t\t\t\t\t\t@make -C ${LIB_DIR} fclean\n")
+file.write("\t\t\t\t\t\t@${RM} ${NAME} ${NAME_B}\n")
+file.write("\t\t\t\t\t\t@echo \"\\nPrograms The libft and " + word + " have been deleted\\n\"\n\n")
+
+file.write("re:\t\tfclean all\n\n")
+file.write(".PHONY:           all     clean   fclean  re\n")
 file.close()
 
 print("Les couleurs sont a rajouter a la main !!!\n")
